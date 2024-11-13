6095c44f44b8aa2efc129dfaebcf8957c8fd7fba
Priya Sadh <priya.sadh@accenture.com>
2024-11-13 16:02:48 +0000

    added basic CRUD endpoints

--- src/main/java/sadh/priya/organizr/run/Run.java
+++ src/main/java/sadh/priya/organizr/run/Run.java
@@ -2,11 +2,22 @@ package sadh.priya.organizr.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
-){}
+){
+	public Run {
+		if(!completedOn.isAfter(startedOn)) {
+			throw new IllegalArgumentException("Completed On must be after the Started On");
+		}
+	}
+}
--- src/main/java/sadh/priya/organizr/run/RunController.java
+++ src/main/java/sadh/priya/organizr/run/RunController.java
@@ -1,13 +1,58 @@
 package sadh.priya.organizr.run;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
-	@GetMapping("/hello")
-	String home() {
-		return "Hello, Organisers!";
-	};
+	private final RunRepository runRepository;
+	
+	public RunController(RunRepository runRepository) {
+		this.runRepository = runRepository;
+	}
+	
+	@GetMapping("")
+	List<Run> findAll() {
+		return runRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	Run findById(@PathVariable Integer id) {
+		Optional<Run> run = runRepository.findById(id);
+		
+		if(run.isEmpty()) {
+			throw new RunNotFoundException();
+		}
+		
+		return run.get();
+	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("")
+	void create(@RequestBody Run run) {
+		runRepository.create(run);
+	}
+	
+	@PutMapping("/{id}")
+	void update(@PathVariable Integer id, @RequestBody Run run) {
+		runRepository.update(run, id);
+	}
+	
+	@DeleteMapping("/{id}")
+	void delete(@PathVariable Integer id) {
+		runRepository.delete(id);
+	}
 }
